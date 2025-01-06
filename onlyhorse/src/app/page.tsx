09b91e3f90b3568ba4c282c2be1daa5ac8b21c3f
primalkarma <115176571+primalkarma@users.noreply.github.com>
2025-01-06 12:58:24 +0000

    added assets

--- onlyhorse/src/app/page.tsx
+++ onlyhorse/src/app/page.tsx
@@ -1,9 +1,13 @@
 import Image from "next/image";
+import HomeScreen from "./components/home/home-screen/HomeScreen";
+import AuthScreen from "./components/home/auth-screen/AuthScreen";
 
 export default function Home() {
+  const user = true;
   return (
     <main>
-      <h1>Only Horse</h1>
+      <h1 className="text-primary">Only Horse</h1>
+      {user ? (<HomeScreen/>) : (<AuthScreen/>)}
       
     </main>
   );
